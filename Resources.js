f8920a00c7997d3b8908b10c8d15aad05bd8be99
1998issu-beep <1998issu@gmail.com>
2026-09-24 17:34:59 +0000

    Create Resources.js

---
+++ Resources.js
@@ -0,0 +1,43 @@
+const resources = [
+
+  {
+    class: "Basic 7",
+    subject: "Computing",
+    term: "First Term",
+    week: "Week 1",
+    topic: "Introduction to Computing",
+    plan: "Basic7-Computing-Week1-Lesson-Plan.pdf",
+    notes: "Basic7-Computing-Week1-Lesson-Notes.pdf"
+  },
+
+  {
+    class: "Basic 7",
+    subject: "Computing",
+    term: "First Term",
+    week: "Week 2",
+    topic: "Computer Hardware",
+    plan: "Basic7-Computing-Week2-Lesson-Plan.pdf",
+    notes: "Basic7-Computing-Week2-Lesson-Notes.pdf"
+  },
+
+  {
+    class: "Basic 8",
+    subject: "Computing",
+    term: "First Term",
+    week: "Week 1",
+    topic: "Computer Networks",
+    plan: "Basic8-Computing-Week1-Lesson-Plan.pdf",
+    notes: "Basic8-Computing-Week1-Lesson-Notes.pdf"
+  },
+
+  {
+    class: "Basic 9",
+    subject: "Computing",
+    term: "First Term",
+    week: "Week 1",
+    topic: "Information and Communication Technology",
+    plan: "Basic9-Computing-Week1-Lesson-Plan.pdf",
+    notes: "Basic9-Computing-Week1-Lesson-Notes.pdf"
+  }
+
+];
